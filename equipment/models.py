971cd9407d1ea28f2864b45d85631cba65767ba1
CodeSaint00 <codesaint00@gmail.com>
2026-09-24 11:49:46 +0100

    changed the model to remove the serial neccesity of the equipment addition

--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 class Equipment(models.Model):
@@ -10,9 +11,14 @@ class Equipment(models.Model):
     location = models.CharField(max_length=200)
     name = models.CharField(max_length=150)
     category = models.CharField(max_length=100)
-    serial_number = models.CharField(max_length=100, unique=True)
+    serial_number = models.CharField(max_length=100, unique=True, blank=True)
     condition = models.CharField(max_length=30, choices=Condition.choices, default=Condition.OPERATIONAL)
     installed_date = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.serial_number:
+            self.serial_number = f"EQ-{uuid.uuid4().hex[:8].upper()}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.serial_number})"
