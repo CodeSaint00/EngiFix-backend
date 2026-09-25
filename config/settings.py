cc770eeb5ad2e0c9cf8476f7db7dd72794d595fe
CodeSaint00 <codesaint00@gmail.com>
2026-09-25 09:25:29 +0100

    Add explicit logging so errors show in Render logs

--- config/settings.py
+++ config/settings.py
@@ -172,3 +172,29 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = "accounts.User"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
