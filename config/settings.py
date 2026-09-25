fa408679d7e72bc54f11136265c45389a37c5f06
CodeSaint00 <codesaint00@gmail.com>
2026-09-25 11:16:10 +0100

    added in settings foe fis manifest

--- config/settings.py
+++ config/settings.py
@@ -101,11 +101,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WHITENOISE_MANIFEST_STRICT = False
 
 # Password validation
