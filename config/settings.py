cc79f5c3937700d4793db455dafb81ea2b1fc0bb
CodeSaint00 <codesaint00@gmail.com>
2026-09-25 11:07:13 +0100

    made a change to settings for whitenoise

--- config/settings.py
+++ config/settings.py
@@ -105,6 +105,8 @@ STORAGES = {
     },
 }
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
